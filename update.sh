9300ad9910e1a54de6863fc724bc787333b23486
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2021-01-11 19:46:58 +0300

    Update the updater script

--- update.sh
+++ update.sh
@@ -3,6 +3,5 @@
 kks sync -f
 find -name statement.md | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/\1-\2.md:' | xargs -n2 cp
 find -name statement.html | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/html/\1-\2.html:' | xargs -n2 cp
-sed -E -i 's:\[/?code\]::g' statements/*.md
 sed -E -i 's:SID=[a-z0-9]+:SID=00000000:g' statements/*.md statements/html/*.html
 git add statements && git commit -m "Update to $(date "+%d.%m.%y %R")"
