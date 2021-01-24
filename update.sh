0b2cc6871288345963838472f6e739da73c7a0e1
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2021-01-24 03:38:59 +0300

    update updater (make hidden blocks collapsible)

--- update.sh
+++ update.sh
@@ -4,4 +4,6 @@ kks sync -f
 find -name statement.md | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/\1-\2.md:' | xargs -n2 cp
 find -name statement.html | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/html/\1-\2.html:' | xargs -n2 cp
 sed -E -i 's:SID=[a-z0-9]+:SID=00000000:g' statements/*.md statements/html/*.html
+sed -E -i 's:\*\*\[hidden text\]\*\*.*:<details>\n<summary>hidden text</summary>\n:' statements/*.md
+sed -E -i 's:\*\*\[end of hidden text\]\*\*.*:</details>:' statements/*.md
 git add statements && git commit -m "Update to $(date "+%d.%m.%y %R")"
