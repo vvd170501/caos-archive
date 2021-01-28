1aba3795c089060d23222f221d31268bd45adad9
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2021-01-28 15:02:36 +0300

    allow updating only last n contests

--- update.sh
+++ update.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-kks sync -f
+lastcnt=3
+
+if [[ "$1" == "last" ]]; then
+    contests=$(kks top -l $lastcnt | head -n 1 | python -c "print(' '.join([e.strip() for e in input().split('|')[-$lastcnt:]]))")
+    # may sync extra contests if names contain spaces (rewrite?)
+    kks sync -f $contests
+else
+    kks sync -f
+fi
+
 find -name statement.md | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/\1-\2.md:' | xargs -n2 cp
 find -name statement.html | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/html/\1-\2.html:' | xargs -n2 cp
 sed -E -i 's:SID=[a-z0-9]+:SID=00000000:g' statements/*.md statements/html/*.html
