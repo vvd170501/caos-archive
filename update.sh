419be13cb956dced1c02a66b3c3df1149c69199d
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2021-02-05 11:42:04 +0300

    keep deleted statements

--- update.sh
+++ update.sh
@@ -10,8 +10,15 @@ else
     kks sync -f
 fi
 
-find -name statement.md | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/\1-\2.md:' | xargs -n2 cp
-find -name statement.html | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/html/\1-\2.html:' | xargs -n2 cp
+function copy () {
+    grep -Fqv "Statement is not available" "$1" && cp "$1" "$2"  # don't overwrite old statements if they are deleted
+    test -f "$2" || cp $1 $2  # if statement is not available, but there aren't any old versions
+}
+
+export -f copy
+
+find -name statement.md | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/\1-\2.md:' | xargs -n2 bash -c 'copy "${0}" "${1}"'
+find -name statement.html | sed -E 's:[^/]*/([^/]+)/([^/]+).*:\0 ./statements/html/\1-\2.html:' | xargs -n2 bash -c 'copy "${0}" "${1}"'
 sed -E -i 's:SID=[a-z0-9]+:SID=00000000:g' statements/*.md statements/html/*.html
 sed -E -i 's:\*\*\[hidden text\]\*\*.*:<details>\n<summary>hidden text</summary>\n:' statements/*.md
 sed -E -i 's:\*\*\[end of hidden text\]\*\*.*:</details>:' statements/*.md
